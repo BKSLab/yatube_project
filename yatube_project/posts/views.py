3ba42a7578e5c8cc74b279c76999a600453de57b
BKSLab <bks2408@mail.ru>
2023-01-17 15:35:20 +0400

    Добавлены urls и view-функции

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница тестового проекта')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Привет, я страница группы или что-то такое {slug}')
+    pass
