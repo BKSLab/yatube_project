8fdc0bb453c6ecdcf87746880088f77f816fd548
BKSLab <bks2408@mail.ru>
2023-01-29 22:25:53 +0400

    Community project before code review

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+# from django.template import loader
+# from django.http import Http404
+from .models import Post, Group
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse('Главная страница тестового проекта')
+    title = 'Это главная страница проекта Yatube!'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    return HttpResponse(f'Привет, я страница группы или что-то такое {slug}')
-    
+    title = (f'Здесь будет информация о группах проекта Yatube {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
--- yatube_project/yatube_project/settings.py
+++ yatube_project/yatube_project/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube_project.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
